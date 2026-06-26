4e35d8e8b2adfad655de3f438fed698c37b9aa82
Whitney Shisia <shisiawhitney215@gmail.com>
2026-06-26 12:53:43 +0300

    edited cofigurations

--- afyalink-api/app/core/config.py
+++ afyalink-api/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     TEST_DATABASE_URL: Optional[str] = None
     
     # Security
-    SECRET_KEY: str
+    SECRET_KEY: str = "becedb80ea9d0f5617b0933e83186531ef7f91d6e8ce08aa8c372b7cd16d9a3c"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
@@ -18,24 +18,24 @@ class Settings(BaseSettings):
     REDIS_URL: str = "redis://localhost:6379/0"
     
     # Email
-    SMTP_HOST=smtp.gmail.com
-    SMTP_PORT=465
-    SMTP_USER=shisiawhitney215@gmail.com
-    SMTP_PASSWORD=bnxynidwkcajqqua
-    SMTP_FROM=noreply@afyalink.com
+    SMTP_HOST: str = "smtp.gmail.com"
+    SMTP_PORT: int = 465
+    SMTP_USER: str = "shisiawhitney215@gmail.com"
+    SMTP_PASSWORD: str = "bnxynidwkcajqqua"
+    SMTP_FROM: str = "noreply@afyalink.com"
         
     # M-Pesa
-    MPESA_ENVIRONMENT=sandbox
-    MPESA_CONSUMER_KEY=ynr0QGt5EDAHkBafrJvyFWjFKqJTQrvTWwDTGlk7WwZO2QMz
-    MPESA_CONSUMER_SECRET=YjnP6vnTkRbdGDkxEtY6lbFyThHhk9eA0xBvry4HqbKfHUZ2tRsaRXzJR6duN4Zj
-    MPESA_PASSKEY=your-passkey
-    MPESA_SHORTCODE=174379
-    MPESA_CALLBACK_URL=https://your-domain.com/api/v1/payments/mpesa-callback
+    MPESA_ENVIRONMENT: str = "sandbox"
+    MPESA_CONSUMER_KEY: str = "ynr0QGt5EDAHkBafrJvyFWjFKqJTQrvTWwDTGlk7WwZO2QMz"
+    MPESA_CONSUMER_SECRET: str = "YjnP6vnTkRbdGDkxEtY6lbFyThHhk9eA0xBvry4HqbKfHUZ2tRsaRXzJR6duN4Zj"
+    MPESA_PASSKEY: str = "your-passkey"
+    MPESA_SHORTCODE: str = "174379"
+    MPESA_CALLBACK_URL: str = "https://your-domain.com/api/v1/payments/mpesa-callback"
 
     
     # Stripe
-    STRIPE_SECRET_KEY=sk_test_51Th3Ah5g17PE31PyClg8H6iGX9UmcK3kChsmrzULWDiAYq22mC3L2gAMZi4W8KjIY2cvKsFRmFMwFqcUKNOcM02c00lDdkyPrQ
-    STRIPE_WEBHOOK_SECRET=mock
+    STRIPE_SECRET_KEY: str = "sk_test_51Th3Ah5g17PE31PyClg8H6iGX9UmcK3kChsmrzULWDiAYq22mC3L2gAMZi4W8KjIY2cvKsFRmFMwFqcUKNOcM02c00lDdkyPrQ"
+    STRIPE_WEBHOOK_SECRET: str = "mock"
     
     # Cloudinary
     # CLOUDINARY_CLOUD_NAME: str
